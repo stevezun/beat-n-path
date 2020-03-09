9cd534bf3aab40d5cd91977e46c5e05d607514eb
Steven Zuniga <szuniga@cnm.edu>
2020-03-09 12:06:12 -0600

    Finally corrected viewmodel and navhost issue. App will now compile and display on emulator.

--- app/src/main/java/edu/cnm/deepdive/beatnpath/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/beatnpath/controller/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.cnm.deepdive.beatnpath.controller;
 
 import android.os.Bundle;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -8,22 +10,49 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import edu.cnm.deepdive.beatnpath.R;
+import edu.cnm.deepdive.beatnpath.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+  private NavController navController;
+  private BottomNavigationView navigator;
+  private NavOptions navOptions;
+  private MainViewModel viewModel;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate( savedInstanceState );
     setContentView( R.layout.activity_main );
     BottomNavigationView navView = findViewById( R.id.nav_view );
-    // Passing each menu ID as a set of Ids because each
-    // menu should be considered as top level destinations.
+    setupNavigation();  findViewById( R.id.navigation_beatNpath );
+    setupNavigation();
     AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-        R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications )
+        R.id.navigation_user, R.id.navigation_beatNpath, R.id.navigation_music )
         .build();
     NavController navController = Navigation.findNavController( this, R.id.nav_host_fragment );
     NavigationUI.setupActionBarWithNavController( this, navController, appBarConfiguration );
     NavigationUI.setupWithNavController( navView, navController );
+  }
+
+  private void setupNavigation() {
+    navOptions = new NavOptions.Builder()
+        .setPopUpTo(R.id.mobile_navigation, true)
+        .build();
+    AppBarConfiguration appBarConfiguration =
+        new AppBarConfiguration.Builder(R.id.navigation_music, R.id.navigation_user)
+            .build();
+    navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    navigator = findViewById(R.id.mobile_navigation);
+    };
+
+  private void navigateTo(int itemId) {
+    if (navController.getCurrentDestination().getId() != itemId) {
+      navController.navigate(itemId, null, navOptions);
+      if (navigator.getSelectedItemId() != itemId) {
+        navigator.setSelectedItemId(itemId);
+      }
+    }
   }
 
 }
---
+++ app/src/main/java/edu/cnm/deepdive/beatnpath/viewmodel/MainViewModel.java
@@ -0,0 +1,5 @@
+package edu.cnm.deepdive.beatnpath.viewmodel;
+
+public class MainViewModel {
+
+}
